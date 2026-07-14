8d7a5828a9d73bdaa66dd47be6b7c880a52c06f6
alfredopc26 <calfredoperez26@gmail.com>
2026-07-14 15:17:21 -0500

    bug nomina

--- app/(app)/nomina/page.tsx
+++ app/(app)/nomina/page.tsx
@@ -24,7 +24,7 @@ interface ControlSearchParams {
 const PAGE_SIZE = 15;
 
 const LEGAL_PARAMETERS_COLUMNS =
-  "id, year, minimum_wage, transport_allowance, overtime_day_factor, overtime_night_factor, night_surcharge_factor, holiday_day_factor, holiday_night_surcharge_factor, holiday_night_factor, holiday_overtime_day_factor, holiday_overtime_night_factor, lunch_subsidy_per_day, health_employer_percent, health_employee_percent, pension_employer_percent, pension_employee_percent, fsp_employee_percent, caja_compensacion_percent, icbf_percent, sena_percent, cesantias_percent, cesantias_interes_percent, vacaciones_percent, primas_percent, arl_level_1_percent, arl_level_2_percent, arl_level_3_percent, arl_level_4_percent, arl_level_5_percent, incapacidad_percent, created_at, updated_at";
+  "id, year, minimum_wage, transport_allowance, overtime_day_factor, overtime_night_factor, night_surcharge_factor, holiday_day_factor, holiday_night_surcharge_factor, holiday_night_factor, holiday_overtime_day_factor, holiday_overtime_night_factor, lunch_subsidy_per_day, lunch_break_start, lunch_break_end, shift_start, shift_end, grace_minutes, weekly_legal_hours, health_employer_percent, health_employee_percent, pension_employer_percent, pension_employee_percent, fsp_employee_percent, caja_compensacion_percent, icbf_percent, sena_percent, cesantias_percent, cesantias_interes_percent, vacaciones_percent, primas_percent, arl_level_1_percent, arl_level_2_percent, arl_level_3_percent, arl_level_4_percent, arl_level_5_percent, incapacidad_percent, created_at, updated_at";
 
 export default async function ControlPage({
   searchParams,
